d30a93a6040288d7ca59d4b0a270810de319d4a2
TanjilHaque <thisismydesign9@gmail.com>
2025-06-17 08:46:00 +0600

    read file done

--- index.js
+++ index.js
@@ -6,20 +6,23 @@ const path = require("path");
 let command = process.argv[2];
 let folderName, fileName, data, folderPath, targetPath;
 
+
+if (process.argv[3].includes(".")) {
+  fileName = process.argv[3];
+  data = process.argv[4];
+  folderPath = __dirname;
+  targetPath = path.join(folderPath, fileName);
+} else {
+  folderName = process.argv[3];
+  fileName = process.argv[4];
+  data = process.argv[5];
+  folderPath = path.join(__dirname, folderName);
+  targetPath = path.join(folderPath, fileName);
+}
+
+
 switch (command) {
   case "write":
-    if (process.argv[3].includes(".")) {
-      fileName = process.argv[3];
-      data = process.argv[4];
-      folderPath = __dirname;
-      targetPath = path.join(folderPath, fileName);
-    } else {
-      folderName = process.argv[3];
-      fileName = process.argv[4];
-      data = process.argv[5];
-      folderPath = path.join(__dirname, folderName);
-      targetPath = path.join(folderPath, fileName);
-    }
     if (!fs.existsSync(folderPath)) {
       fs.mkdirSync(folderPath);
     }
@@ -34,6 +37,15 @@ switch (command) {
     break;
 
   case "read": {
+    //read file takes 3 arguments --> path, encoding, callback
+    fs.readFile(targetPath, "utf-8", (err, data)=>{
+      if(err){
+        console.log("There was an error reading the file, ", err);
+      }else{
+        console.log(`File Contents:\n${data}`);
+        console.log("File read successfully");
+      }
+    })
     break;
   }
 
