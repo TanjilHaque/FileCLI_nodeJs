c894c7441abb157d1feb8301a36a36b33a32294c
TanjilHaque <thisismydesign9@gmail.com>
2025-06-22 01:10:48 +0600

    rename file done

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //Project Start
 
+const { profileEnd } = require("console");
 const fs = require("fs");
 const path = require("path");
 
 let command = process.argv[2];
-let folderName, fileName, data, folderPath, targetPath;
+let folderName, fileName, newFileName, data, folderPath, targetPath, oldPath, newPath;
 
-if (process.argv[3] && process.argv[3].includes(".")) {
+if ((command === "write" || command === "read") && process.argv[3] && process.argv[3].includes(".")) {
   fileName = process.argv[3];
   data = process.argv[4];
   folderPath = __dirname;
@@ -14,7 +15,23 @@ if (process.argv[3] && process.argv[3].includes(".")) {
 } else if (command === "list") {
   folderName = process.argv[3];
   targetPath = folderName ? path.join(__dirname, folderName) : __dirname;
-} else {
+}
+else if(command === 'rename'){
+  if(process.argv[3] && process.argv[3].includes(".")){
+    fileName = process.argv[3];
+    newFileName = process.argv[4];
+    oldPath = path.join(__dirname, fileName);
+    newPath = path.join(__dirname, newFileName)
+  }else{
+    folderName = process.argv[3];
+    fileName = process.argv[4];
+    newFileName = process.argv[5];
+    oldPath = path.join(__dirname,folderName,fileName);
+    newPath = path.join(__dirname, folderName, newFileName);
+  }
+
+}
+else {
   folderName = process.argv[3];
   fileName = process.argv[4];
   data = process.argv[5];
@@ -64,7 +81,15 @@ switch (command) {
     });
     break;
   }
+  // takes 3 arguments
   case "rename": {
+    fs.rename(oldPath, newPath, (err)=>{
+      if(err){
+        console.log("There has been an error renaming the file");
+      }else{
+        console.log("File rename successful")
+      }
+    })
     break;
   }
   case "replace": {
